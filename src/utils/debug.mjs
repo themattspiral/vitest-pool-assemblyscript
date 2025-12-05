1c806ddc0ee1bbef0767c24c57fe2ec05a8906b0
Matt Ritter <matthew.d.ritter@gmail.com>
2025-12-05 17:02:36 -0600

    chore: handle callback to defer logging work only if debug enabled in debug helper function

--- src/utils/debug.mjs
+++ src/utils/debug.mjs
@@ -28,7 +28,14 @@ export function setDebugMode(debugEnabled) {
 export function debug(...args) {
   const state = debugStorage.getStore();
   if (state?.debug || process.env.DEBUG) {
-    console.log(...args);
+    // if first arg is a function, execute it and then print the result
+    if (args.length > 0 && typeof args[0] === 'function') {
+      const result = args[0]();
+      const rest = args.length > 1 ? args.slice(1) : [];
+      console.log(String(result), ...rest);
+    } else {
+      console.log(...args);
+    }
   }
 }
 
