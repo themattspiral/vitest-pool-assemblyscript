6e6044a53462c028db05d0a3b925ddd4a4e3929e
Matt Ritter <matthew.d.ritter@gmail.com>
2026-01-30 19:37:42 -0600

    docs(readme): clarify user and framework imports

--- test/assembly/user-imports.as.test.ts
+++ test/assembly/user-imports.as.test.ts
@@ -3,15 +3,15 @@ import {
   runUserFunction,
   runOtherUserFunction,
   runParseIntStringFunction,
-} from './helpers/user-import-wrapper';
+} from './helpers/user-import-wrapper.help';
 
-describe("default \"env\" environment", () => {
+describe("default \"env\" module", () => {
   test("user imported function is executed", () => {
     expect(runUserFunction(2)).toBe(12);
   });
 });
 
-describe("\"customUserEnv\" environment", () => {
+describe("\"customUserModule\" module", () => {
   test("user imported function is executed", () => {
     expect(runOtherUserFunction(2)).toBe(20);
   });
